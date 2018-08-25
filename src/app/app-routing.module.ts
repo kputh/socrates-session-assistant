237cd4905ae7c650fbe7dbe9f6c2fa8f24a87a85
Kai Puth <kai.puth@akquinet.de>
2018-08-25 16:27:18 +0200

    Minor cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {AddSessionComponent} from './sessions/add-session/add-session.component';
-
 const routes: Routes = [
 ];
 
